9eeaa181525203951434ee11b5248d9216057e48
s009desh <63408926+s009desh@users.noreply.github.com>
2023-10-30 03:00:54 +0530

    Update MedicureApplicationTests.java

--- src/test/java/com/project/staragile/MedicureApplicationTests.java
+++ src/test/java/com/project/staragile/MedicureApplicationTests.java
@@ -6,26 +6,24 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.project.staragile.MedicureService;
-
 @SpringBootTest
 class MedicureApplicationTests {
 
-	@Autowired
-	MedicureService doctorService;
-	
-	@Test
-	void contextLoads() {
-		
-	}
-	
-	@Test
-	void testService() {
-	  assertEquals(doctorService.sayHello(), "Hello from Doctor Shubham");
-	}
-	
-	
-	void testService() {
-		  assertEquals(MedicureService.sayHello(), "priya");
-		}
+    @Autowired
+    MedicureService doctorService;
+
+    @Test
+    void contextLoads() {
+        
+    }
+
+    @Test
+    void testService() {
+        assertEquals(doctorService.sayHello(), "Hello from Doctor Shubham");
+    }
+
+    @Test
+    void anotherTestService() {
+        assertEquals(MedicureService.sayHello(), "priya");
+    }
 }
